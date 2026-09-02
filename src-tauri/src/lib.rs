532bad5001c1a4b5978d4081a3d5aac4642267bf
Simon Cossar <scossar3@gmail.com>
2026-09-01 20:22:14 -0700

    initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,18 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let context: tauri::Context<tauri::Wry> = tauri::generate_context!();
+    #[cfg(target_os = "linux")]
+    glib::set_prgname(Some(context.config().identifier.as_str()));
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
